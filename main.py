880a7fa1e98e96d0e0ac6fc3066bc56e4e2e2cd7
randheer <randheer.dakshana15@gmail.com>
2024-02-17 12:12:24 +0530

    project is setup now

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWSError, jwt
 from passlib.context import CryptContext
+from pydantic_core.core_schema import BoolSchema
 from dotenv import load_dotenv
 
-app = FastAPI()
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, ".env"))
@@ -16,7 +16,47 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-print(SECRET_KEY)
+fake_db = {
+    "tim": {
+        "username": "ravi",
+        "full_name": "ravi coder",
+        "email": "ravi@xyz.come",
+        "hashed_password": "",
+        "disabled": False,
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    username: str
+    email: str
+    fullname: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    """password verification"""
+    return pwd_context.verify(plain_password, hashed_password)
 
 
 class Data(BaseModel):

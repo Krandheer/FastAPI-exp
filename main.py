420be774676b3c7784664f448ae9a07829316957
randheer <randheer.dakshana15@gmail.com>
2024-02-17 11:58:58 +0530

    using .env resolved

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import os
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime, timedelta
+from jose import JWSError, jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
 
 app = FastAPI()
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, ".env"))
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+print(SECRET_KEY)
 
 
 class Data(BaseModel):
